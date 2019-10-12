746e6f6e6a87d7c2eda2e91da6563e99f9f00998
bartosz.bukowski21 <bartosz.bukowski21@gmail.com>
2019-10-12 13:23:05 +0200

    Stworzenie oraz podstawowa konfiguracja projektu.

---
+++ EstateAgency/EstateAgency/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule { }
--- EstateAgency/EstateAgency/Startup.cs
+++ EstateAgency/EstateAgency/Startup.cs
@@ -44,7 +44,18 @@ namespace EstateAgency
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = (context) =>
+                {
+                    // Disable caching for all static files.
+                    context.Context.Response.Headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
+                    context.Context.Response.Headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
+                    context.Context.Response.Headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
+
+                }
+            });
+
             app.UseSpaStaticFiles();
 
             app.UseMvc(routes =>
